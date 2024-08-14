5f2ca4ad5f4ece9e3e512e4d2854874ba8a7fdbf
Luis Defago <defagoluis5@gmail.com>
2024-08-14 18:52:55 -0300

    prueba deploy con vercel2

--- backend/src/utils/connect.js
+++ backend/src/utils/connect.js
@@ -1,6 +1,15 @@
-const {Sequelize} = require('sequelize')
-require('dotenv').config()
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
+});
 
-module.exports = sequelize
+module.exports = sequelize;
