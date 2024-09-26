2bb29ab55719e6eea18c4e6d6bf5f41876cd006a
Luis Defago <defagoluis5@gmail.com>
2024-09-26 13:43:51 -0300

    configuracion para deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Directorio donde se colocarán los archivos del build
+  },
+  server: {
+    // Configuración opcional para desarrollo local, ajusta según lo necesites
+    port: 3000,
+    open: true,  // Abre el navegador automáticamente al iniciar el servidor
+  },
+});
